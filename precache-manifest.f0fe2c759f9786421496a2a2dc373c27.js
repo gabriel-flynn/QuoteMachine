fac9e795707a8ab7ad17aa070bc082c5dab6cffd
Gabriel Flynn <gm.flynn99@yahoo.com>
2020-03-18 22:33:12 -0500

    Updates

---
+++ precache-manifest.f0fe2c759f9786421496a2a2dc373c27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6998c9b2601d3656eb970fd496233a2",
+    "url": "/QuoteMachine/index.html"
+  },
+  {
+    "revision": "c6789322e253d92e0773",
+    "url": "/QuoteMachine/static/css/main.74af650a.chunk.css"
+  },
+  {
+    "revision": "22e0edc1d6df5387dbc8",
+    "url": "/QuoteMachine/static/js/2.e7d07e6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/QuoteMachine/static/js/2.e7d07e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6789322e253d92e0773",
+    "url": "/QuoteMachine/static/js/main.37b7c781.chunk.js"
+  },
+  {
+    "revision": "9d278ef56e9ad6959a5e",
+    "url": "/QuoteMachine/static/js/runtime-main.13bfaf2a.js"
+  },
+  {
+    "revision": "11ce4888745d4fea421981def3353317",
+    "url": "/QuoteMachine/static/media/donut.11ce4888.jpg"
+  }
+]);
